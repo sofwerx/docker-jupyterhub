a85e3cc4e0d9e5977bb555f9328c99bfbac43140
Ian Blenke <ian@blenke.com>
2018-10-14 02:16:29 -0400

    Switching from tightvnc to xorg + x11vnc, enabling glx along the way

--- jupyter-notebook/jupyter_notebook_config.py
+++ jupyter-notebook/jupyter_notebook_config.py
@@ -15,8 +15,11 @@ c.NotebookApp.open_browser = False
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
 
+#c.NBNoVNC.vnc_command = "xinit -- /usr/bin/Xtightvnc :{display} -geometry {geometry} -depth {depth} -auth /home/jovyan/.Xauthority"
+c.NBNoVNC.vnc_command = "xinit -- /usr/bin/Xorg :{display} +extension GLX +extension RANDR +extension RENDER -listen tcp -config /etc/X11/xorg.conf -auth /home/jovyan/.Xauthority"
+c.NBNoVNC.helper_command = "x11vnc -xkb -rfbport {vnc_port}"
 c.NBNoVNC.websockify_command = "/opt/conda/envs/py2/bin/websockify --web {novnc_directory} --heartbeat {heartbeat} {port} localhost:{vnc_port}"
-c.NBNoVNC.vnc_command = "xinit -- /usr/bin/Xtightvnc :{display} -geometry {geometry} -depth {depth} -auth /home/jovyan/.Xauthority"
+
 c.NBNoVNC.geometry = "1280x768"
 
 # Generate a self-signed certificate
